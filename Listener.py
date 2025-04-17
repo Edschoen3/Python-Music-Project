bfcbf18955ae5931a039557001d72709f9e3cf30
ethanschoen <edschoen3@gmail.com>
2025-04-17 13:06:45 -0500

    Began code.
    switching to VSCode

--- Listener.py
+++ Listener.py
@@ -1,9 +1,17 @@
-pip install sounddevice scipy shazamio
 import asyncio
 import sounddevice as sd
-from scipy.io.wavfile import write
+# from scipy.io.wavfile import write
 from shazamio import Shazam
 from datetime import datetime
 import csv
 import time
 import os
+
+AUDIO_FILENAME = "clip.wav"
+CSV_FILENAME = "songs.csv"
+RECORD_SECONDS = 10
+SAMPLE_RATE = 44100
+
+def record_audio():
+    print(f"[{datetime.now().isoformat()}] Recording: ")
+    audio = sd.rec(int(duration * fs), samplerate = fs, channels=2)
